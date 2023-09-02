abc62b3632a8dcc86002c66c0082cb7bad95aa9b
manoj <mbasnet012@gmail.com>
2023-09-01 23:11:27 -0500

    react practice

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css"; // Import the CSS file
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="title">CSS and JS</h1>
+      <p className="description">
+        This is a task for CSS Styling in a React Project with Vite
+      </p>
+    </div>
+  );
+}
+
+export default App;
